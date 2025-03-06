603b86a5c46a4a5f98ba4cfcdb32eb6f1e671e0f
fruitsalad266 <fruitsalad266@gmail.com>
2025-03-06 13:38:30 -0800

    updated about

--- components/articles/About.js
+++ components/articles/About.js
@@ -21,18 +21,23 @@ const About = () => {
             <h3 style={{marginTop: '-15px'}}>UW Interfraternity Council</h3>
             <h4 style={{marginTop: '-15px'}}>Director of Public Relations</h4>
             <ul style={{marginTop: '-5px'}}>
-                <li>Represent 2,200+ Greek Men on social media <a href="https://instagram.com/uwifc" target="_blank">platforms</a></li>
+                <li>Represent 2,200+ Greek Men on social media <a href="https://instagram.com/uwifc" target="_blank">platforms </a> and ensure  
+                    <a href="https://static1.squarespace.com/static/570ec8a48259b53bf09d3da4/t/57a013d0e3df28965b49a563/1470108625800/Theme%3AAdvertising+Statement.pdf" target="_blank"> policy</a> compliance
+                </li>
                 <li>Redesigned and maintained <a href="https://uwifc.com" target="_blank">official website</a> to drive recruitment numbers </li>
                 <li>Won an <a href="https://aflv.org/award-winners#mms" target="_blank">award</a> for best IFC website</li>
             </ul>
             </span>
-
-            <div>
+            
             <h3 style={{marginTop: '-15px'}}><a href="https://montlakeconsulting.com" target="_blank">Montlake Consulting Group</a></h3>
             <h4 style={{marginTop: '-15px'}}>Consultant</h4>
-            <li style={{marginTop: '-5px'}}>Work with clients on engagements to explore  niche markets through data analysis</li>
-            <hr style={{marginTop: '15px'}}></hr>
+            <ul><li style={{marginTop: '-5px'}}>Work with clients on engagements to explore  niche markets through data analysis</li></ul>
 
+            <h3 style={{marginTop: '-15px'}}>Pi Kappa Alpha Fraternity</h3>
+            <h4 style={{marginTop: '-15px'}}>Head of Public Relations</h4>
+            <li style={{marginTop: '-5px'}}>Represent the fraternity <a href="https://instagram.com/uwpike" target="_blank">online</a></li>
+            
+            <hr style={{marginTop: '15px'}}></hr>
             <h2 style={{marginTop: '-15px'}}>Former</h2>
 
             <h3>Husky Taekwondo</h3>
@@ -41,7 +46,7 @@ const About = () => {
                 <li>Grew <a href="https://instagram.com/uwhuskytkd" target="_blank">Instagram</a> following from 600 to 900+ in 3 months of tenure</li>
                 <li>Directed, edited and shot a <a href="" target="_blank">recruitment video</a> reaching over 10k accounts and increasing club sign-ups by 80%</li>
             </ul>
-            </div>
+            
         </>
     )
 }
--- components/articles/Intro.js
+++ components/articles/Intro.js
@@ -8,11 +8,25 @@ const Intro = () => {
             {/* <span className="image main"><Image src="/static/images/matt1.jpg" height={500} width={500} alt="" /></span> */}
             <p>Hi! I'm a third-year student at the University of Washington, Seattle, studying both Informatics (SDE) and Business (Operations Supply Chain). 
                     I also work at <a href="https://www.washington.edu/research/compliance/" target="_blank">UW Research Compliance </a>
-                     and am the current Director of PR for <a href="https://uwifc.com" target="blank">UW IFC.</a>
+                     as a data analyst and am the current Director of PR for <a href="https://uwifc.com" target="blank">UW IFC.</a>
                 </p>
             <p>I'm highly experienced in research, data, marketing, full stack software development, database management, 
                 ML and AI et cetera. I'm also experienced in DJing. </p>
             <p>The best way to contact me is through my email: <a href="mailto:mattcng9@uw.edu">mattcng9 (at) uw dot edu</a></p>
+            <hr></hr>
+            <h3>Fast Facts</h3>
+            <ul>
+                <li><b>Computer Languages:</b> Python, Java, JavaScript, C++, Bash, TypeScript, HTML, SQL, R, JSX</li>
+                <li><b>Human Languages:</b> English, Vietnamese, Mandarin Chinese, Spanish</li>
+                <li><b>Tools:</b> Git, Pandas, Seaborn, plotly, scikit-learn, React, FastAPI, Next.JS, Node.JS, PyTorch, Jupyter, Agile framework, QuantConnect</li>
+                <li><b>Software:</b><a href="https://www.blackmagicdesign.com/products/davinciresolve" target="_blank"> Davinci Resolve</a>
+                    , Figma, Canva, Adobe Photoshop, Tableau, Excel</li>
+                <li><b>Interests:</b> Data science, fullstack development, statistics, quantitative finance. 
+                <a href="https://en.wikipedia.org/wiki/Illenium" target="_blank"> EDM</a>, College Football, weightlifting, game theory.
+                </li>
+                <li><b>SAT:</b> 1590</li>
+            </ul>
+
         </>
     )
 }
